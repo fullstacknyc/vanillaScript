ff3c8bca38299b48e88ae81a125f73cdc2b9aa5a
fullstacknyc <jgomezval@icloud.com>
2025-03-01 21:23:57 -0500

    e

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const greeting = 'Hello, world!';
 
-greeting.attributes = 'id="greeting"';
-body.style = `align-items: center;font-family: Segoe UI, sans-serif; display: flex; height: 50vh; justify-content: center; margin: 0;`;
 document.write(greeting);
 
 document.body.appendChild(greeting);
