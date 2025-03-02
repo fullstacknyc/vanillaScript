e19c854b8efda96401df583f5f63902042463f35
fullstacknyc <jgomezval@icloud.com>
2025-03-01 21:30:04 -0500

    f

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const greeting = 'Hello, world!';
 document.write(greeting);
 
 document.body.appendChild(greeting);
+
+document.body.style.alignContent = 'center';
+
+document.body.style.backgroundColor = 'black';
+
+document.body.style.color = 'white';
