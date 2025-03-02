b18c0a6d93abc451fe799179695b193220cf3b85
fullstacknyc <jgomezval@icloud.com>
2025-03-02 01:26:58 -0500

    clock and date

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-const myTitle = document.createElement('title');
-myTitle.textContent = 'pipboy 3000';
-document.head.appendChild(myTitle);
+const hud [
+    time: "new Date()";
 
-document.body.style.backgroundColor = 'black';
-document.body.style.color = 'white';
-
-const myDiv = document.createElement('div');
-document.body.appendChild(myDiv);
-
-myDiv.style.fontSize = '50px';
-myDiv.style.fontWeight = 'bold';
-myDiv.style.fontFamily = 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif';
-myDiv.style.position = 'absolute';
-myDiv.style.top = '0';
-myDiv.style.right = '0';
-
-const style = document.createElement('style');
-style.innerHTML = `@keyframes slideInFromRight {
-    0% { transform: translateX(100%); }
-    100% { transform: translateX(0); }
-}`;
-document.head.appendChild(style);
-
-myDiv.style.animation = 'slideInFromRight 1s ease-in-out';
-myDiv.style.animationFillMode = 'forwards';
-myDiv.style.animationDelay = '1s';
-
-function displayTime() {
-    myDiv.textContent = new Date().toLocaleTimeString();
-}
-
-setInterval(displayTime, 1000);
+]
