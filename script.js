c78b8efa1a84ccc5a572d601c4e2555efc6f7a2e
fullstacknyc <jgomezval@icloud.com>
2025-03-23 08:47:47 -0400

    ux design

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Cloning tech boxes for infinite scroll
     const container = document.querySelector('.tech-boxes-container');
     const techBoxes = document.querySelectorAll('.tech-box');
     techBoxes.forEach(box => container.appendChild(box.cloneNode(true)));
 
+    // Button to change background color
     document.getElementById('changeColorBtn').addEventListener('click', function() {
         document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     });
